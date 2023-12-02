62433cc47c70f436304d4766d46da0887f0b2bab
vbaga <vbaga@DESKTOP-2B0B0FP>
2023-12-02 14:05:17 +0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace calculator1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.n5 = new System.Windows.Forms.Button();
+            this.n6 = new System.Windows.Forms.Button();
+            this.n9 = new System.Windows.Forms.Button();
+            this.gamravleba = new System.Windows.Forms.Button();
+            this.minusi = new System.Windows.Forms.Button();
+            this.n2 = new System.Windows.Forms.Button();
+            this.n7 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.n3 = new System.Windows.Forms.Button();
+            this.wertili = new System.Windows.Forms.Button();
+            this.n0 = new System.Windows.Forms.Button();
+            this.washla = new System.Windows.Forms.Button();
+            this.pliusi = new System.Windows.Forms.Button();
+            this.toloba = new System.Windows.Forms.Button();
+            this.n8 = new System.Windows.Forms.Button();
+            this.n4 = new System.Windows.Forms.Button();
+            this.gayopa = new System.Windows.Forms.Button();
+            this.gamotvla = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // n5
+            // 
+            this.n5.BackColor = System.Drawing.Color.Silver;
+            this.n5.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n5.ForeColor = System.Drawing.Color.Red;
+            this.n5.Location = new System.Drawing.Point(117, 214);
+            this.n5.Name = "n5";
+            this.n5.Size = new System.Drawing.Size(105, 85);
+            this.n5.TabIndex = 0;
+            this.n5.Text = "5";
+            this.n5.UseVisualStyleBackColor = false;
+            this.n5.Click += new System.EventHandler(this.n5_Click);
+            // 
+            // n6
+            // 
+            this.n6.BackColor = System.Drawing.Color.Silver;
+            this.n6.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n6.ForeColor = System.Drawing.Color.Red;
+            this.n6.Location = new System.Drawing.Point(228, 214);
+            this.n6.Name = "n6";
+            this.n6.Size = new System.Drawing.Size(105, 85);
+            this.n6.TabIndex = 0;
+            this.n6.Text = "6";
+            this.n6.UseVisualStyleBackColor = false;
+            this.n6.Click += new System.EventHandler(this.n6_Click);
+            // 
+            // n9
+            // 
+            this.n9.BackColor = System.Drawing.Color.Silver;
+            this.n9.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n9.ForeColor = System.Drawing.Color.Red;
+            this.n9.Location = new System.Drawing.Point(228, 305);
+            this.n9.Name = "n9";
+            this.n9.Size = new System.Drawing.Size(105, 85);
+            this.n9.TabIndex = 0;
+            this.n9.Text = "9";
+            this.n9.UseVisualStyleBackColor = false;
+            this.n9.Click += new System.EventHandler(this.n9_Click);
+            // 
+            // gamravleba
+            // 
+            this.gamravleba.BackColor = System.Drawing.Color.Silver;
+            this.gamravleba.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gamravleba.ForeColor = System.Drawing.Color.Green;
+            this.gamravleba.Location = new System.Drawing.Point(339, 123);
+            this.gamravleba.Name = "gamravleba";
+            this.gamravleba.Size = new System.Drawing.Size(105, 85);
+            this.gamravleba.TabIndex = 0;
+            this.gamravleba.Text = "×";
+            this.gamravleba.UseVisualStyleBackColor = false;
+            // 
+            // minusi
+            // 
+            this.minusi.BackColor = System.Drawing.Color.Silver;
+            this.minusi.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minusi.ForeColor = System.Drawing.Color.Green;
+            this.minusi.Location = new System.Drawing.Point(339, 214);
+            this.minusi.Name = "minusi";
+            this.minusi.Size = new System.Drawing.Size(105, 85);
+            this.minusi.TabIndex = 0;
+            this.minusi.Text = "-";
+            this.minusi.UseVisualStyleBackColor = false;
+            // 
+            // n2
+            // 
+            this.n2.BackColor = System.Drawing.Color.Silver;
+            this.n2.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n2.ForeColor = System.Drawing.Color.Red;
+            this.n2.Location = new System.Drawing.Point(117, 123);
+            this.n2.Name = "n2";
+            this.n2.Size = new System.Drawing.Size(105, 85);
+            this.n2.TabIndex = 0;
+            this.n2.Text = "2";
+            this.n2.UseVisualStyleBackColor = false;
+            this.n2.Click += new System.EventHandler(this.n2_Click);
+            // 
+            // n7
+            // 
+            this.n7.BackColor = System.Drawing.Color.Silver;
+            this.n7.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n7.ForeColor = System.Drawing.Color.Red;
+            this.n7.Location = new System.Drawing.Point(6, 305);
+            this.n7.Name = "n7";
+            this.n7.Size = new System.Drawing.Size(105, 85);
+            this.n7.TabIndex = 0;
+            this.n7.Text = "7";
+            this.n7.UseVisualStyleBackColor = false;
+            this.n7.Click += new System.EventHandler(this.n7_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Silver;
+            this.button4.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.Red;
+            this.button4.Location = new System.Drawing.Point(6, 123);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(105, 85);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "1";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.n1);
+            // 
+            // n3
+            // 
+            this.n3.BackColor = System.Drawing.Color.Silver;
+            this.n3.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n3.ForeColor = System.Drawing.Color.Red;
+            this.n3.Location = new System.Drawing.Point(228, 123);
+            this.n3.Name = "n3";
+            this.n3.Size = new System.Drawing.Size(105, 85);
+            this.n3.TabIndex = 0;
+            this.n3.Text = "3";
+            this.n3.UseVisualStyleBackColor = false;
+            this.n3.Click += new System.EventHandler(this.n3_Click);
+            // 
+            // wertili
+            // 
+            this.wertili.BackColor = System.Drawing.Color.Silver;
+            this.wertili.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wertili.ForeColor = System.Drawing.Color.Red;
+            this.wertili.Location = new System.Drawing.Point(6, 396);
+            this.wertili.Name = "wertili";
+            this.wertili.Size = new System.Drawing.Size(105, 85);
+            this.wertili.TabIndex = 0;
+            this.wertili.Text = ".";
+            this.wertili.UseVisualStyleBackColor = false;
+            // 
+            // n0
+            // 
+            this.n0.BackColor = System.Drawing.Color.Silver;
+            this.n0.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n0.ForeColor = System.Drawing.Color.Red;
+            this.n0.Location = new System.Drawing.Point(117, 396);
+            this.n0.Name = "n0";
+            this.n0.Size = new System.Drawing.Size(105, 85);
+            this.n0.TabIndex = 0;
+            this.n0.Text = "0";
+            this.n0.UseVisualStyleBackColor = false;
+            // 
+            // washla
+            // 
+            this.washla.BackColor = System.Drawing.Color.Silver;
+            this.washla.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.washla.ForeColor = System.Drawing.Color.DarkGreen;
+            this.washla.Location = new System.Drawing.Point(228, 396);
+            this.washla.Name = "washla";
+            this.washla.Size = new System.Drawing.Size(105, 85);
+            this.washla.TabIndex = 0;
+            this.washla.Text = "C";
+            this.washla.UseVisualStyleBackColor = false;
+            // 
+            // pliusi
+            // 
+            this.pliusi.BackColor = System.Drawing.Color.Silver;
+            this.pliusi.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pliusi.ForeColor = System.Drawing.Color.Green;
+            this.pliusi.Location = new System.Drawing.Point(339, 305);
+            this.pliusi.Name = "pliusi";
+            this.pliusi.Size = new System.Drawing.Size(105, 85);
+            this.pliusi.TabIndex = 0;
+            this.pliusi.Text = "+";
+            this.pliusi.UseVisualStyleBackColor = false;
+            // 
+            // toloba
+            // 
+            this.toloba.BackColor = System.Drawing.Color.Silver;
+            this.toloba.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toloba.ForeColor = System.Drawing.Color.Black;
+            this.toloba.Location = new System.Drawing.Point(339, 396);
+            this.toloba.Name = "toloba";
+            this.toloba.Size = new System.Drawing.Size(105, 85);
+            this.toloba.TabIndex = 0;
+            this.toloba.Text = "=";
+            this.toloba.UseVisualStyleBackColor = false;
+            // 
+            // n8
+            // 
+            this.n8.BackColor = System.Drawing.Color.Silver;
+            this.n8.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n8.ForeColor = System.Drawing.Color.Red;
+            this.n8.Location = new System.Drawing.Point(117, 305);
+            this.n8.Name = "n8";
+            this.n8.Size = new System.Drawing.Size(105, 85);
+            this.n8.TabIndex = 0;
+            this.n8.Text = "8";
+            this.n8.UseVisualStyleBackColor = false;
+            this.n8.Click += new System.EventHandler(this.n8_Click);
+            // 
+            // n4
+            // 
+            this.n4.BackColor = System.Drawing.Color.Silver;
+            this.n4.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n4.ForeColor = System.Drawing.Color.Red;
+            this.n4.Location = new System.Drawing.Point(6, 214);
+            this.n4.Name = "n4";
+            this.n4.Size = new System.Drawing.Size(105, 85);
+            this.n4.TabIndex = 0;
+            this.n4.Text = "4";
+            this.n4.UseVisualStyleBackColor = false;
+            this.n4.Click += new System.EventHandler(this.n4_Click);
+            // 
+            // gayopa
+            // 
+            this.gayopa.BackColor = System.Drawing.Color.Silver;
+            this.gayopa.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gayopa.ForeColor = System.Drawing.Color.Green;
+            this.gayopa.Location = new System.Drawing.Point(339, 32);
+            this.gayopa.Name = "gayopa";
+            this.gayopa.Size = new System.Drawing.Size(105, 85);
+            this.gayopa.TabIndex = 0;
+            this.gayopa.Text = "÷";
+            this.gayopa.UseVisualStyleBackColor = false;
+            // 
+            // gamotvla
+            // 
+            this.gamotvla.Font = new System.Drawing.Font("SimSun-ExtB", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gamotvla.Location = new System.Drawing.Point(6, 32);
+            this.gamotvla.Multiline = true;
+            this.gamotvla.Name = "gamotvla";
+            this.gamotvla.Size = new System.Drawing.Size(327, 85);
+            this.gamotvla.TabIndex = 0;
+            this.gamotvla.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 510);
+            this.Controls.Add(this.gamotvla);
+            this.Controls.Add(this.gayopa);
+            this.Controls.Add(this.n4);
+            this.Controls.Add(this.n3);
+            this.Controls.Add(this.n8);
+            this.Controls.Add(this.n7);
+            this.Controls.Add(this.toloba);
+            this.Controls.Add(this.n2);
+            this.Controls.Add(this.pliusi);
+            this.Controls.Add(this.washla);
+            this.Controls.Add(this.minusi);
+            this.Controls.Add(this.n5);
+            this.Controls.Add(this.n0);
+            this.Controls.Add(this.gamravleba);
+            this.Controls.Add(this.wertili);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.n9);
+            this.Controls.Add(this.n6);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "კალკულატორი";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button n5;
+        private System.Windows.Forms.Button n6;
+        private System.Windows.Forms.Button n9;
+        private System.Windows.Forms.Button gamravleba;
+        private System.Windows.Forms.Button minusi;
+        private System.Windows.Forms.Button n2;
+        private System.Windows.Forms.Button n7;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button n3;
+        private System.Windows.Forms.Button wertili;
+        private System.Windows.Forms.Button n0;
+        private System.Windows.Forms.Button washla;
+        private System.Windows.Forms.Button pliusi;
+        private System.Windows.Forms.Button toloba;
+        private System.Windows.Forms.Button n8;
+        private System.Windows.Forms.Button n4;
+        private System.Windows.Forms.Button gayopa;
+        private System.Windows.Forms.TextBox gamotvla;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void n1(object sender, EventArgs e)
+        {   if (gamotvla.Text=="0"){
+             gamotvla.Text="1";
+
+            }
+            else {
+                gamotvla.Text+="1";
+
+            }
+        }
+
+        private void n2_Click(object sender, EventArgs e)
+        {
+            if (gamotvla.Text == "0")
+            {
+                gamotvla.Text = "2";
+
+            }
+            else
+            {
+                gamotvla.Text += "2";
+
+            }
+        }
+
+        private void n3_Click(object sender, EventArgs e)
+        {
+            if (gamotvla.Text == "0")
+            {
+                gamotvla.Text = "3";
+
+            }
+            else
+            {
+                gamotvla.Text += "3";
+
+            }
+        }
+
+        private void n4_Click(object sender, EventArgs e)
+        {
+            if (gamotvla.Text == "0")
+            {
+                gamotvla.Text = "4";
+
+            }
+            else
+            {
+                gamotvla.Text += "4";
+
+            }
+        }
+
+        private void n5_Click(object sender, EventArgs e)
+        {
+            if (gamotvla.Text == "0")
+            {
+                gamotvla.Text = "5";
+
+            }
+            else
+            {
+                gamotvla.Text += "5";
+
+            }
+        }
+
+        private void n6_Click(object sender, EventArgs e)
+        {
+            if (gamotvla.Text == "0")
+            {
+                gamotvla.Text = "6";
+
+            }
+            else
+            {
+                gamotvla.Text += "6";
+
+            }
+        }
+
+        private void n7_Click(object sender, EventArgs e)
+        {
+            if (gamotvla.Text == "0")
+            {
+                gamotvla.Text = "7";
+
+            }
+            else
+            {
+                gamotvla.Text += "7";
+
+            }
+        }
+
+        private void n8_Click(object sender, EventArgs e)
+        {
+            if (gamotvla.Text == "0")
+            {
+                gamotvla.Text = "8";
+
+            }
+            else
+            {
+                gamotvla.Text += "8";
+
+            }
+        }
+
+        private void n9_Click(object sender, EventArgs e)
+        {
+            if (gamotvla.Text == "0")
+            {
+                gamotvla.Text = "9";
+
+            }
+            else
+            {
+                gamotvla.Text += "9";
+
+            }
+        }
+    }
+}
